5c4d70a5d68752fe9f2173b5d0b9bb19592f7025
Antonio Hernandez <antonio.hzc@gmail.com>
2016-07-05 17:26:00 +0200

    App class - DBFlow

--- app/src/main/java/hzc/antonio/carlocator/CarLocatorApp.java
+++ app/src/main/java/hzc/antonio/carlocator/CarLocatorApp.java
@@ -2,13 +2,21 @@ package hzc.antonio.carlocator;
 
 import android.app.Application;
 
+import com.raizlabs.android.dbflow.config.FlowConfig;
+import com.raizlabs.android.dbflow.config.FlowManager;
+
 public class CarLocatorApp extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        initDB();
         initFirebase();
         initModules();
+    }
+
+    private void initDB() {
+        FlowManager.init(new FlowConfig.Builder(this).build());
     }
 
     private void initFirebase() {
@@ -17,5 +25,15 @@ public class CarLocatorApp extends Application {
 
     private void initModules() {
 
+    }
+
+    @Override
+    public void onTerminate() {
+        DBTearDown();
+        super.onTerminate();
+    }
+
+    private void DBTearDown() {
+        FlowManager.destroy();
     }
 }
