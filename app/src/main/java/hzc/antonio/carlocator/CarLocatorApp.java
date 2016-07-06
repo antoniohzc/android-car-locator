b6ef5145f5295c54d0c945ff15f0081ac4304421
Antonio Hernandez <antonio.hzc@gmail.com>
2016-07-06 13:37:41 +0200

    App class - Firebase, SharedPreferences

--- app/src/main/java/hzc/antonio/carlocator/CarLocatorApp.java
+++ app/src/main/java/hzc/antonio/carlocator/CarLocatorApp.java
@@ -2,10 +2,14 @@ package hzc.antonio.carlocator;
 
 import android.app.Application;
 
+import com.firebase.client.Firebase;
 import com.raizlabs.android.dbflow.config.FlowConfig;
 import com.raizlabs.android.dbflow.config.FlowManager;
 
 public class CarLocatorApp extends Application {
+    private static final String SHARED_PREFERENCES_NAME = "UserPrefs";
+    private static final String SHARED_PREFERENCES_EMAIL_KEY = "email";
+    private static final String FIREBASE_URL = "https://android-car-locator-hzc.firebaseio.com/" ;
 
     @Override
     public void onCreate() {
@@ -20,7 +24,7 @@ public class CarLocatorApp extends Application {
     }
 
     private void initFirebase() {
-
+        Firebase.setAndroidContext(this);
     }
 
     private void initModules() {
@@ -35,5 +39,13 @@ public class CarLocatorApp extends Application {
 
     private void DBTearDown() {
         FlowManager.destroy();
+    }
+
+    public String getSharedPrefName() {
+        return SHARED_PREFERENCES_NAME;
+    }
+
+    public String getSharedPrefEmailKey() {
+        return SHARED_PREFERENCES_EMAIL_KEY;
     }
 }
