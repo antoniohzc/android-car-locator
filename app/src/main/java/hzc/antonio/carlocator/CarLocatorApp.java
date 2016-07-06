7239dee46cf064189a1ebac43ea5aaf6dc97e2b5
Antonio Hernandez <antonio.hzc@gmail.com>
2016-07-06 16:44:31 +0200

    App class and Domain DI

--- app/src/main/java/hzc/antonio/carlocator/CarLocatorApp.java
+++ app/src/main/java/hzc/antonio/carlocator/CarLocatorApp.java
@@ -6,10 +6,15 @@ import com.firebase.client.Firebase;
 import com.raizlabs.android.dbflow.config.FlowConfig;
 import com.raizlabs.android.dbflow.config.FlowManager;
 
+import hzc.antonio.carlocator.domain.di.DomainModule;
+
 public class CarLocatorApp extends Application {
     private static final String SHARED_PREFERENCES_NAME = "UserPrefs";
     private static final String SHARED_PREFERENCES_EMAIL_KEY = "email";
     private static final String FIREBASE_URL = "https://android-car-locator-hzc.firebaseio.com/" ;
+
+    private CarLocatorAppModule carLocatorAppModule;
+    private DomainModule domainModule;
 
     @Override
     public void onCreate() {
@@ -28,7 +33,8 @@ public class CarLocatorApp extends Application {
     }
 
     private void initModules() {
-
+        carLocatorAppModule = new CarLocatorAppModule(this);
+        domainModule = new DomainModule(FIREBASE_URL);
     }
 
     @Override
@@ -47,5 +53,9 @@ public class CarLocatorApp extends Application {
 
     public String getSharedPrefEmailKey() {
         return SHARED_PREFERENCES_EMAIL_KEY;
+    }
+
+    public String getFirebaseUrl() {
+        return FIREBASE_URL;
     }
 }
--- app/src/main/java/hzc/antonio/carlocator/libs/di/LibsComponent.java
+++ app/src/main/java/hzc/antonio/carlocator/libs/di/LibsComponent.java
@@ -3,6 +3,7 @@ package hzc.antonio.carlocator.libs.di;
 import javax.inject.Singleton;
 
 import dagger.Component;
+import hzc.antonio.carlocator.CarLocatorAppModule;
 
 @Singleton
 @Component(modules = {LibsModule.class, CarLocatorAppModule.class})
