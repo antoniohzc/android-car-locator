3fbe649bd42c1e4fbc2badf0bdd18832f0ec801b
Antonio Hernandez <antonio.hzc@gmail.com>
2016-07-07 12:57:14 +0200

    Adapter and activity

--- app/src/main/java/hzc/antonio/carlocator/main/ui/MainActivity.java
+++ app/src/main/java/hzc/antonio/carlocator/main/ui/MainActivity.java
@@ -1,15 +1,144 @@
 package hzc.antonio.carlocator.main.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import hzc.antonio.carlocator.CarLocatorApp;
 import hzc.antonio.carlocator.R;
+import hzc.antonio.carlocator.login.ui.LoginActivity;
+import hzc.antonio.carlocator.main.MainPresenter;
+import hzc.antonio.carlocator.main.ui.adapters.MainSectionsPagerAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainView,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    @BindView(R.id.container) CoordinatorLayout container;
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.lblTitle) TextView lblTitle;
+    @BindView(R.id.lblUser) TextView lblUser;
+    @BindView(R.id.tabLayout) TabLayout tabLayout;
+    @BindView(R.id.viewPager) ViewPager viewPager;
+
+    MainPresenter presenter;
+    SharedPreferences sharedPreferences;
+    MainSectionsPagerAdapter adapter;
+
+    private CarLocatorApp app;
+    private GoogleApiClient googleApiClient;
+    private Location lastKnownLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        app = (CarLocatorApp) getApplication();
+        setupInjection();
+        setupNavigation();
+        setupGoogleApiClient();
+
+        presenter.onCreate();
+    }
+
+    private void setupInjection() {
+
+    }
+
+    private void setupNavigation() {
+        String email = sharedPreferences.getString(app.getSharedPrefEmailKey(), getString(R.string.main_appbar_user));
+        lblUser.setText(email);
+        setSupportActionBar(toolbar);
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupGoogleApiClient() {
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        presenter.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_logout) {
+            logout();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        presenter.logout();
+        sharedPreferences.edit().clear().commit();
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                | Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
