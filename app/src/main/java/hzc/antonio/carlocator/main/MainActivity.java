9ad0d448aae78fe0f82f6449a1834f1ac77e81e6
Antonio Hernandez <antonio.hzc@gmail.com>
2016-07-07 12:19:30 +0200

    Layout

---
+++ app/src/main/java/hzc/antonio/carlocator/main/MainActivity.java
@@ -0,0 +1,15 @@
+package hzc.antonio.carlocator.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hzc.antonio.carlocator.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
